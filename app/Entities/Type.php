40338fe5436a1581566526d1ef9ef17c32dc3803
Wayne <s1410432005@gms.nutc.edu.tw>
2017-07-03 23:42:29 +0800

    [feature]新增漫畫分類資料表

---
+++ app/Entities/Type.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Entities\Type;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $types = ['未分類', '少女', '百合', '日常', '玄幻', '遊戲', '治癒', '科幻', '搞笑', '鬼怪', '勵志', '運動', '格鬥', '熱血', '愛情', '校園', '耽美', '偽娘', '冒險', '職場', '後宮', '親情', '戰爭', '懸疑', '偵探', '奇幻', '魔法', '恐怖', '萌系', '歷史', '美食', '競技', '機戰'];
+        foreach ($types as $type) {
+	        Type::create(['name' => $type]);
+        }
     }
 }
